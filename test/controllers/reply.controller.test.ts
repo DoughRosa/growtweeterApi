02e378564c068ef0d703b00648f88a715a52caba
Rafael Henkel <rafael0henkel1@gmail.com>
2024-10-30 22:19:22 -0300

    feat: finishe reply test

--- test/controllers/reply.controller.test.ts
+++ test/controllers/reply.controller.test.ts
@@ -169,5 +169,138 @@ describe("este teste do reply controller", () => {
 
   //////////////////////////UPDATE REPLIES//////////////////////////
 
+  test("deveria retornar 404 ao não encontrar o reply para atualizalo", async () => {
+    const response = await request(app)
+      .put(`/replies/3e962168-36cc-4acc-902c-a00db14f3f20`) //ID INEXISTENTE
+      .send({ content: "retweet atualizado com sucesso" })
+      .set("Accept", "application/json")
+      .set("Authorization", `bearer ${token}`);
+
+    expect(response.status).toEqual(404);
+  });
+
+  test("deveria retornar 500 ao acontecer um erro de banco de dados", async () => {
+    expect.assertions(3);
+
+    const responseCreateReply = await request(app)
+      .post("/replies")
+      .send({ tweetId, content: "retweet com sucesso" })
+      .set("Accept", "application/json")
+      .set("Authorization", `bearer ${token}`);
+
+    expect(responseCreateReply.status).toEqual(200);
+
+    const response = await request(app)
+      .put(`/replies/${responseCreateReply.body.data.id}`)
+      .send({ content: 123 })
+      .set("Accept", "application/json")
+      .set("Authorization", `bearer ${token}`);
+
+    expect(response.status).toEqual(500);
+
+    const responseTweet = await db.replies.delete({
+      where: {
+        id: responseCreateReply.body.data.id,
+      },
+    });
+
+    expect(responseTweet).toHaveProperty("id");
+  });
+
+  test("deveria retornar 400 ao tentar atualizar um reply sem content", async () => {
+    expect.assertions(3);
+
+    const responseCreateReply = await request(app)
+      .post("/replies")
+      .send({ tweetId, content: "retweet com sucesso" })
+      .set("Accept", "application/json")
+      .set("Authorization", `bearer ${token}`);
+
+    expect(responseCreateReply.status).toEqual(200);
+
+    const response = await request(app)
+      .put(`/replies/${responseCreateReply.body.data.id}`)
+      .send({ content: null })
+      .set("Accept", "application/json")
+      .set("Authorization", `bearer ${token}`);
+
+    expect(response.status).toEqual(400);
+
+    const responseTweet = await db.replies.delete({
+      where: {
+        id: responseCreateReply.body.data.id,
+      },
+    });
+
+    expect(responseTweet).toHaveProperty("id");
+  });
+
+  test("deveria retornar 200 ao atualizar um reply", async () => {
+    expect.assertions(3);
+
+    const responseCreateReply = await request(app)
+      .post("/replies")
+      .send({ tweetId, content: "retweet com sucesso" })
+      .set("Accept", "application/json")
+      .set("Authorization", `bearer ${token}`);
+
+    expect(responseCreateReply.status).toEqual(200);
+
+    const response = await request(app)
+      .put(`/replies/${responseCreateReply.body.data.id}`)
+      .send({ content: "retweet atualizado com sucesso" })
+      .set("Accept", "application/json")
+      .set("Authorization", `bearer ${token}`);
+
+    expect(response.status).toEqual(200);
+
+    console.log(response.body);
+
+    const responseTweet = await db.replies.delete({
+      where: {
+        id: responseCreateReply.body.data.id,
+      },
+    });
+
+    expect(responseTweet).toHaveProperty("id");
+  });
+
   //////////////////////////DELETE REPLIES//////////////////////////
+
+  test("deveria retornar 200 ao tentar deletar um unico reply", async () => {
+    expect.assertions(2);
+
+    const responseCreateReply = await request(app)
+      .post("/replies")
+      .send({ tweetId, content: "retweet com sucesso" })
+      .set("Accept", "application/json")
+      .set("Authorization", `bearer ${token}`);
+
+    expect(responseCreateReply.status).toEqual(200);
+
+    const response = await request(app)
+      .delete(`/replies/${responseCreateReply.body.data.id}`)
+      .set("Accept", "application/json")
+      .set("Authorization", `bearer ${token}`);
+
+    expect(response.status).toEqual(200);
+  });
+
+  test("deveria retornar 404 ao não encontrar o reply ao tentar deletar", async () => {
+    const response = await request(app)
+      .delete(`/replies/3e962168-36cc-4acc-902c-a00db14f3f20`) //ID INEXISTENTE
+      .set("Accept", "application/json")
+      .set("Authorization", `bearer ${token}`);
+
+    expect(response.status).toEqual(404);
+  });
+
+  test("deveria retornar 500 ao passar um parametro invalido ao tentar deletar um reply", async () => {
+    const response = await request(app)
+      .delete(`/replies/invalidparameter`)
+      .set("Accept", "application/json")
+      .set("Authorization", `bearer ${token}`);
+
+    expect(response.status).toEqual(500);
+  });
 });
